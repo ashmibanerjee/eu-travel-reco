bac1e084f3c03fad9f3293c8ec0eceb99ed0c432
Ashmi Banerjee <ashmibanerjee1411@gmail.com>
2024-10-07 17:19:23 +0200

    updates to inference

--- src/eval/inference.py
+++ src/eval/inference.py
@@ -1,5 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from ..utils.loaders import load_data
+import sys
+
+sys.path.append("../../")
+from src.utils.loaders import load_data
+
 
 def load_model():
     output_dir = "../../models/gemma-2b-travel-qa"
@@ -7,10 +11,13 @@ def load_model():
     finetuned_model = AutoModelForCausalLM.from_pretrained(output_dir, load_in_4bit=True, device_map="auto")
     return tokenizer, finetuned_model
 
+
 def load_test_data():
     repo_name = "ashmib/wikivoyage-eu-cities-qa"
     test_data = load_data(repo_name, data_files=None, is_public=True, split_name="test")
+    test_data.format(type="pandas")
     return test_data
+
 
 def generate_answers(query, tokenizer, finetuned_model):
     inputs = tokenizer(query, return_tensors="pt")
@@ -21,4 +28,15 @@ def generate_answers(query, tokenizer, finetuned_model):
 def main():
     test_data = load_test_data()
     print("Test data loaded")
+    tokenizer, finetuned_model = load_model()
+    responses = []
+    for i in range(len(test_data)[:2]):
+        query = test_data[i]["prompt"]
+        response = generate_answers(query, tokenizer, finetuned_model)
+        response.append(response)
+    test_data["llm_response"] = responses
+    test_data.save_to_disk("../../data/eval/gemma-2b-travel-qa-answers.csv")
 
+
+if __name__ == "__main__":
+    main()
